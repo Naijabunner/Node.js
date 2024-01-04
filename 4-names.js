e6fd3bdaab8db8ea97d6ad06431b8160329a664e
Naiabunner <Okechukwudaniel12@gmail.com>
2024-01-04 03:53:16 +0000

    updates

---
+++ 4-names.js
@@ -0,0 +1,9 @@
+//local
+const  secret ="SUPER SECRET"
+//share
+const john ="john"
+const peter ="peter"
+
+module.exports={john,peter}
+//since the key is the same with the value , we dont need to put a key 
+// console.log(module)
---
+++ 6-alternative.js
@@ -0,0 +1,4 @@
+module.exports.item =["item1", "Item2"]
+const Person={
+    name:"Bob"
+}
