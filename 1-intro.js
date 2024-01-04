abcaa53095b11925eb62d09cbe625293b729da88
Naiabunner <Okechukwudaniel12@gmail.com>
2024-01-04 03:05:31 +0000

    1 intro

---
+++ 1-intro.js
@@ -0,0 +1,11 @@
+const amount = 12
+if (amount<10){
+    console.log("small number")
+}else{
+    console.log("LArge number")
+
+}
+console.log("my FIRST node app")
+
+
+// if you want the file g=Go to terminal then type node App.js
