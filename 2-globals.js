c30408ce7c63430c865e1de7268577a85ef7ec1c
Naiabunner <Okechukwudaniel12@gmail.com>
2024-01-04 03:14:42 +0000

    globals

---
+++ 2-globals.js
@@ -0,0 +1,12 @@
+// GLobals - No WINDOW!!
+
+// __dirname - path to current directory
+// __filename - file name
+// require -function to use modules (commonJs)
+//module- function to use modules
+// modules- info on the current modules
+// process- info of the process being ran
+console.log(__dirname)
+// setInterval(() => {
+    
+// }, interval);
